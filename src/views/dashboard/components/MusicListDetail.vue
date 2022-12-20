2b436c6cafd2d1362d4ce52b2ccd59f7ce8606d4
Willow233 <1642974246@qq.com>
2022-12-21 03:00:26 +0800

    歌单详情

--- src/views/dashboard/components/MusicListDetail.vue
+++ src/views/dashboard/components/MusicListDetail.vue
@@ -1,4 +1,5 @@
 <template>
+    <MusicListTop :playlist="state.playlist"/>
     <div>
         歌单详情页
     </div>
@@ -8,24 +9,25 @@
 import {getListDetail} from '@/api/dashboard'
 import { onMounted ,reactive} from 'vue'
 import { useRoute } from 'vue-router'
+import MusicListTop from './MusicListTop.vue'
 export default{
-    setup(){
+    setup() {
+        components: {
+            MusicListTop;
+        }
         const state = reactive({
             playlist: {}
-        })
-
-        const getDetail = async() =>{
-            let id = useRoute().query.id
-            const res = await getListDetail(id)
-            state.playlist = res.data.playlist
-
-        }
-
+        });
+        const getDetail = async () => {
+            let id = useRoute().query.id;
+            const res = await getListDetail(id);
+            state.playlist = res.data.playlist;
+        };
         onMounted(() => {
-            getDetail()
-        })
-
-        return {state}
-    }
+            getDetail();
+        });
+        return { state };
+    },
+    components: { MusicListTop }
 }
 </script>
